785d56f9a3b092135755edf3fa5a08f0bbac6e23
Manan Singh <manan.shine@gmail.com>
2018-07-25 20:19:18 +0000

    Made in china

---
+++ src/main/java/app/AppController.java
@@ -0,0 +1,21 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AppController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppController.class);
+    }
+
+    @RequestMapping("/hi")
+    public String sayHi() {
+        return "Hello World";
+    }
+
+}
